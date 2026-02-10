945f8b5642f2fd1b441b3caec4e98c589494d213
Ivan Pons Oltra <llimapons@hotmail.com>
2026-02-10 13:51:20 +0100

    convention Plugin

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.jpa)
-    alias(libs.plugins.spring.boot)
-    alias(libs.plugins.spring.dependency.management)
-    alias(libs.plugins.kotlin.spring)
+    id("java-library")
+    id("echo.spring-boot-service")
+    kotlin("plugin.jpa")
 }
 
 group = "com.llimapons"
@@ -14,6 +12,8 @@ repositories {
 }
 
 dependencies {
+    implementation(projects.common)
+
     testImplementation(kotlin("test"))
 }
 
